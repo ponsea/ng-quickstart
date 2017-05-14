c964eed6d997327c0988d2d6b72feee6c82191a4
tike <puodsea@gmail.com>
2017-05-14 20:49:37 +0900

    Use Promise to get heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -80,7 +80,8 @@ export class AppComponent {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    // this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
