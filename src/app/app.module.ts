43c3e51748ea1d8b9feffd67fb3044f77a8ce932
tike <puodsea@gmail.com>
2017-05-15 21:47:42 +0900

    Make it possible to show a hero detail with 'detail/:id' url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
-import { DashboardComponent }         from './dashboard.component';
+import { DashboardComponent }  from './dashboard.component';
 
 @NgModule({
   imports: [
@@ -24,6 +24,9 @@ import { DashboardComponent }         from './dashboard.component';
         path: '',
         redirectTo: '/dashboard',
         pathMatch: 'full'
+      }, {
+        path: 'detail/:id',
+        component: HeroDetailComponent
       }
     ])
   ],
