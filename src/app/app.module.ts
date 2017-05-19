3f89deceb8fcfd1b41f1c2a6289e3dd54eec9263
tike <puodsea@gmail.com>
2017-05-19 20:34:35 +0900

    Use HeroSearchComponent in the dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 import { DashboardComponent }  from './dashboard.component';
+import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
   imports: [
@@ -28,7 +29,8 @@ import { DashboardComponent }  from './dashboard.component';
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   providers: [
     HeroService
